ffb51d6aed1956592f692222febe36e28e154e77
wilsenn <57092110+wilsenn@users.noreply.github.com>
2019-10-29 13:52:57 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,362 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+<!-- <link rel="stylesheet" type="text/css" href="bs/css/bootstrap.css"> -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<link rel="stylesheet" type="text/css" href="animate.css">
+<link rel="stylesheet" type="text/css" href="mycss.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color:#069DC3;">
+  <a class="navbar-brand" href="#">
+  	<img src="img/logkecil.png" width="100" height="50">
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item cla">
+        <a class="nav-link animated bounce" style="color:white; font-weight:bold; margin-left:120px;" href="#">Kelas</a>
+      </li>
+      <li class="nav-item cla2">
+        <a class="nav-link animated bounce" style="color:white; font-weight:bold; margin-left:60px;" href="#">Chat</a>
+      </li>
+      <li class="nav-item">
+        <form class="form-inline">
+   			<input class="form-control jarbut animated bounce" type="search" placeholder="Search" aria-label="Search">
+  		</form>
+      </li>
+    </ul>
+    <form>
+    <span class="navbar-text">
+     		<button type="button" class="btn btn-outline hov animated heartBeat" data-toggle="modal" data-target="#myModal">Login</button>
+     		<button class="btn btn-light reg animated heartBeat">Register</button>
+     	</span>
+     		<!-- the modal -->
+     		<div class="modal fade" id="myModal">
+     			<div class="modal-dialog">
+     				<div class="modal-content">
+     					<!-- modal header -->
+     					<div class="modal-header">
+     						<h4 class="modal-title">Login Form</h4>
+     						<button type="button" class="close" data-dismiss="modal">&times;</button>
+     					</div>
+     					<!-- modal body -->
+     					<div class="modal-body">
+     						<h3 class="sta"> Let's Start </h3>
+     						<p class="sta2"> Change The World With Your Skill </p>
+     						<form class="was-validated">
+     							<div class="form-group">
+     								<input type="text" name="" class="form-control" placeholder="Enter Email" required>
+     								<div class="valid-feedback">Valid.</div>
+     								<div class="invalid-feedback">Please fill out this field</div>
+     							</div>
+     							<div class="form-group">
+     								<input type="password" class="form-control" placeholder="Enter Password" required>
+     								<div class="valid-feedback">Valid.</div>
+     								<div class="invalid-feedback">Please fill out this field</div>
+     							</div>
+     							<div class="form-group form-check">
+     								<label class="form-check-label">
+     									<input type="checkbox" class="form-check-input" name="remember" required> I Agree to Privacy Policy
+     									<div class="valid-feedback">Valid.</div>
+     									<div class="invalid-feedback">Check this checkbox to continue</div>
+     								</label>
+     							</div>
+     							<button type="submit" class="btn btn-primary">Submit</button>
+     						</form>
+     					</div>
+     					<div class="modal-footer">
+     						<button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+     					</div>
+     				</div>
+     			</div>
+     		</div>
+      	</form>
+  </div>
+</nav>
+<div class="jumbotron jumbotron-fluid wall">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<div class="row">
+					<div class="col-sm-6"> </div>
+					<div class="col-sm-6"> 
+						<p class="car"> Cari Guru Les ? </p>
+						<p class="gur"> Dapatkan Guru les anda sekarang !</p>
+						<button class="btn btn-light dap">Dapatkan Aplikasi</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+	<div class="container-fluid">
+		<p class="re"> Rekomendasi </p>
+		<div class="row">
+		<div class="col-sm-4">
+			<div class="col-sm-12">
+				<div class="col-sm-12">
+					<div class="col-sm-12 gar animated bounceIn">
+					<img src="img/dika.png" class="img-fluid rounded-circle mx-auto d-block be">
+					<p class="dik"> Andika </p>
+					<p class="bin"> star </p>
+					<div class="row">
+						<div class="col-sm-6">
+							<div class="col-sm-12 pel">Pelajaran :</div>
+							<div class="col-sm-12 de"> Design, Koding </div>
+							<div class="col-sm-12 jad"> Jadwal : </div>
+							<div class="col-sm-12 de">Sabtu, Minggu</div>
+						</div>
+						<div class="col-sm-6"> 
+							<div class="col-sm-12 pel">Total Murid :</div>
+							<div class="col-sm-12 de">6/10 Murid</div>
+						</div>
+					</div>
+					<center>
+						<button class="btn btn-primary ma">Lihat</button>
+					</center>
+				</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-4">
+			<div class="col-sm-12">
+				<div class="col-sm-12">
+					<div class="col-sm-12 gar animated bounceIn">
+					<img src="img/dika.png" class="img-fluid rounded-circle mx-auto d-block be">
+					<p class="dik"> Andika </p>
+					<p class="bin"> star </p>
+					<div class="row">
+						<div class="col-sm-6">
+							<div class="col-sm-12 pel">Pelajaran :</div>
+							<div class="col-sm-12 de"> Design, Koding </div>
+							<div class="col-sm-12 jad"> Jadwal : </div>
+							<div class="col-sm-12 de">Sabtu, Minggu</div>
+						</div>
+						<div class="col-sm-6"> 
+							<div class="col-sm-12 pel">Total Murid :</div>
+							<div class="col-sm-12 de">6/10 Murid</div>
+						</div>
+					</div>
+					<center>
+						<button class="btn btn-primary ma">Lihat</button>
+					</center>
+				</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-4">
+			<div class="col-sm-12">
+				<div class="col-sm-12">
+					<div class="col-sm-12 gar animated bounceIn">
+					<img src="img/dika.png" class="img-fluid rounded-circle mx-auto d-block be">
+					<p class="dik"> Andika </p>
+					<p class="bin"> star </p>
+					<div class="row">
+						<div class="col-sm-6">
+							<div class="col-sm-12 pel">Pelajaran :</div>
+							<div class="col-sm-12 de"> Design, Koding </div>
+							<div class="col-sm-12 jad"> Jadwal : </div>
+							<div class="col-sm-12 de">Sabtu, Minggu</div>
+						</div>
+						<div class="col-sm-6"> 
+							<div class="col-sm-12 pel">Total Murid:</div>
+							<div class="col-sm-12 de">6/10 Murid</div>
+						</div>
+					</div>
+					<center>
+						<button class="btn btn-primary ma">Lihat</button>
+					</center>
+				</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-sm-12"> <p class="all"><a href="" class="uu">Lihat Semua Guru >></a></p> </div>
+	</div>
+	</div>
+
+	<div class="container-fluid">
+		<p class="re"> Promo </p>
+	</div>
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+    	<div class="container-fluid">
+	    	<div class="row">
+				<div class="col-sm-4 col-4 ma">
+					<div class="col-sm-12 gar2">
+						<p class="se"> Sewa kembali guru anda <br> dan dapatkan diskon <br> <span class="sen">15%</span> </p>
+					</div>
+				</div>
+				<div class="col-sm-4 col-4 ma">
+					<div class="col-sm-12 gar2">
+						<p class="se"> Sewa kembali guru anda <br> dan dapatkan diskon <br> <span class="sen">15%</span> </p>
+					</div>
+				</div>
+				<div class="col-sm-4 col-4 ma">
+					<div class="col-sm-12 gar2">
+						<p class="se"> Sewa kembali guru anda <br> dan dapatkan diskon <br> <span class="sen">15%</span> </p>
+					</div>
+				</div>
+			</div>
+		</div>
+    </div>
+    <div class="carousel-item">
+      <div class="container-fluid">
+	      <div class="row">
+				<div class="col-sm-4 col-4 ma">
+					<div class="col-sm-12 gar2">
+						<p class="se"> Sewa kembali guru anda <br> dan dapatkan diskon <br> <span class="sen">15%</span> </p>
+					</div>
+				</div>
+				<div class="col-sm-4 col-4 ma">
+					<div class="col-sm-12 gar2">
+						<p class="se"> Sewa kembali guru anda <br> dan dapatkan diskon <br> <span class="sen">15%</span> </p>
+					</div>
+				</div>
+				<div class="col-sm-4 col-4 ma">
+					<div class="col-sm-12 gar2">
+						<p class="se"> Sewa kembali guru anda <br> dan dapatkan diskon <br> <span class="sen">15%</span> </p>
+					</div>
+				</div>
+			</div>
+		</div>
+    </div>
+    <div class="carousel-item">
+      <div class="container-fluid">
+	      <div class="row">
+				<div class="col-sm-4 col-4 ma">
+					<div class="col-sm-12 gar2">
+						<p class="se"> Sewa kembali guru anda <br> dan dapatkan diskon <br> <span class="sen">15%</span> </p>
+					</div>
+				</div>
+				<div class="col-sm-4 col-4 ma">
+					<div class="col-sm-12 gar2">
+						<p class="se"> Sewa kembali guru anda <br> dan dapatkan diskon <br> <span class="sen">15%</span> </p>
+					</div>
+				</div>
+				<div class="col-sm-4 col-4 ma">
+					<div class="col-sm-12 gar2">
+						<p class="se"> Sewa kembali guru anda <br> dan dapatkan diskon <br> <span class="sen">15%</span> </p>
+					</div>
+				</div>
+			</div>
+		</div>
+    </div>
+  </div>
+  <!-- <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon ba" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon ba2" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a> -->
+</div>
+<!-- sini -->
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-sm-12">
+				<h3 class="gu animated bounceInLeft"> Apa itu GuruKu </h3>
+			</div>
+			<div class="col-sm-12">
+				<p class="pen"> <span class="ru">GuruKu</span> adalah aplikasi yang dikhususkan untuk masyarakat yang kesulitan dalam  mencari guru les yang berkualitas dengan harga yang terjangkau. Sekarang anda tidak  perlu khawatir lagi karena sekarang <span class="ru">GuruKu</span> hadir untuk memecahkan masalah itu kami tentunya menyediakan guru les yang berkualitas dengan harga yang terjangkau dengan proses yang sangat mudah dan aman. </p>
+			</div>		
+		</div>
+	</div>
+	<div class="container-fluid">
+		<p class="re"> Testimoni </p>
+		<div class="row">
+			<div class="col-sm-4">
+				<div class="col-sm-12 ris animated fadeIn">
+					<img src="img/dika.png" class="img-fluid rounded-circle mx-auto d-block be">
+					<p class="dik"> Andika wiraputra </p>
+					<p class="kot"> Kalimantan Barat </p>
+					<center>
+						<div class="gaga"></div>
+					</center>
+					<p class="sa"> sangat menarik, aplikasi ini sangat membantu saya dalam mencari guru les privat sekarang saya tidak perlu susah-susah lagi mencari guru les cukup melalui hp saya sudah bisa  mencari guru les yang saya inginkan dan guru-guru  yang disediakan juga sangat berkualitas dan tentunya harganya sangat terjangkau ditambah lagi dengan banyak promo yang diberikan </p>
+				</div>
+			</div>
+			<div class="col-sm-4">
+				<div class="col-sm-12 ris animated fadeIn">
+					<img src="img/dika.png" class="img-fluid rounded-circle mx-auto d-block be">
+					<p class="dik"> Andika wiraputra </p>
+					<p class="kot"> Kalimantan Barat </p>
+					<center>
+						<div class="gaga"></div>
+					</center>
+					<p class="sa"> sangat menarik, aplikasi ini sangat membantu saya dalam mencari guru les privat sekarang saya tidak perlu susah-susah lagi mencari guru les cukup melalui hp saya sudah bisa  mencari guru les yang saya inginkan dan guru-guru  yang disediakan juga sangat berkualitas dan tentunya harganya sangat terjangkau ditambah lagi dengan banyak promo yang diberikan </p>
+				</div>
+			</div>
+			<div class="col-sm-4">
+				<div class="col-sm-12 ris animated fadeIn">
+					<img src="img/dika.png" class="img-fluid rounded-circle mx-auto d-block be">
+					<p class="dik"> Andika wiraputra </p>
+					<p class="kot"> Kalimantan Barat </p>
+					<center>
+						<div class="gaga"></div>
+					</center>
+					<p class="sa"> sangat menarik, aplikasi ini sangat membantu saya dalam mencari guru les privat sekarang saya tidak perlu susah-susah lagi mencari guru les cukup melalui hp saya sudah bisa  mencari guru les yang saya inginkan dan guru-guru  yang disediakan juga sangat berkualitas dan tentunya harganya sangat terjangkau ditambah lagi dengan banyak promo yang diberikan </p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-sm-12 foot">
+				<div class="row">
+					<div class="col-sm-3">
+						<ul class="ti">
+							<li class="ut">Informasi Perusahaan</li>
+							<li class="ut0">Tentang Kami</li>
+							<li class="ut2">Karir</li>
+							<li class="ut2">Kontak</li>
+						</ul>
+					</div>
+					<div class="col-sm-3">
+						<ul class="ti">
+							<li class="ut">Pelajari lebih lanjut</li>
+							<li class="ut0">Fitur</li>
+							<li class="ut2">Kisah Sukses</li>
+							<li class="ut2">Cara Transaksi</li>
+							<li class="ut2">Menjadi Pengajar</li>
+							<li class="ut2">Aplikasi Ponsel</li>
+						</ul>
+					</div>
+					<div class="col-sm-2">
+						<ul class="ti">
+							<li class="ut">Layanan lainnya</li>
+							<li class="ut0">Instant E-learning</li>
+							<li class="ut2">Bantuan</li>
+						</ul>
+					</div>
+					<div class="col-sm-2">
+						<ul class="ti">
+							<li class="ut">Ikuti Kami</li>
+							<li class="ut0">Facebook</li>
+							<li class="ut2">Instagram</li>
+							<li class="ut2">Twitter</li>
+						</ul>
+					</div>
+				</div>
+			<div class="row">
+				<div class="col-sm-12">
+					<p class="sya">Syarat dan Ketentuan . Kebijakan Privasi</p>
+				</div>
+			</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
